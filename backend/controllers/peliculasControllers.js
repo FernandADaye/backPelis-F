f09a1e09d04c2bcaa084420bee5f8916b179d9ef
Fernanda López <fer.daye.2007@gmail.com>
2024-03-13 14:22:21 -0600

    prueba y error

--- backend/controllers/peliculasControllers.js
+++ backend/controllers/peliculasControllers.js
@@ -1,21 +1,24 @@
 
-const getPeli = (res, req) =>{
-    res.status(200).json({mensaje:'getPeli bien'})
+const getPeli = (req, res) =>{
+    res.status (200).json({mensaje:'todo bien con get peli '})
 }
-const createPeli =(res, req) =>{
-    res.status(201).json({mensaje:'createPeli bien'})
-}
-const   editPeli =(res, req) =>{
-    res.status(201).json({mensaje:'editPeli bien'})
-}
-const deletePeli =(res, req) =>{
-    res.status(200).json({mensaje:'deletePeli bien '})
-}
-
-
+// const getPeli = (res, req) =>{
+    // res.status(200).json({mensaje:'getPeli bien'})
+// }
+// const createPeli =(res, req) =>{
+    // res.status(201).json({mensaje:'createPeli bien'})
+// }
+// const   editPeli =(res, req) =>{
+    // res.status(201).json({mensaje:'editPeli bien'})
+// }
+// const deletePeli =(res, req) =>{
+    // res.status(200).json({mensaje:'deletePeli bien '})
+// }
+// 
+// 
 module.exports = {
     getPeli,
-    createPeli,
-    editPeli,
-    deletePeli,
+    // createPeli,
+    // editPeli,
+    // deletePeli,
 };
