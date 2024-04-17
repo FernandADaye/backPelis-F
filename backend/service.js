f4acfafbb6131ad37a4e89837affbe5bd92713ce
Fernanda López <fer.daye.2007@gmail.com>
2024-04-17 12:16:23 -0600

    Esto a sido un exito y terminamos con CORS✔

--- backend/service.js
+++ backend/service.js
@@ -1,5 +1,6 @@
 const express = require ('express')
 const colors = require('colors')
+const cors = require('cors')
 const conectdb= require('../backend/config/db')
 const dotenv = require ('dotenv').config()
 const port = process.env.PORT || 1975
@@ -8,6 +9,8 @@ conectdb()
 
 // Definir la app con express 
 const app = express()
+// cosr 
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
