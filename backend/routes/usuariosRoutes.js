9231580cee16b7dd4885b1d9aa1b3cac91bb9835
Fernanda López <fer.daye.2007@gmail.com>
2024-04-19 17:31:05 -0600

    cambiando algunas cosas para frontEnd

--- backend/routes/usuariosRoutes.js
+++ backend/routes/usuariosRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const{crearUser,loginUser,datoUser}=require('../controllers/usuariosControllers')
 const { protect } = require('../meddleware/authMeddleware')
-router.post ('/crear', crearUser)
+router.post ('/', crearUser)
 router.post ('/login', loginUser)
 router.get ('/datos',protect, datoUser)
 
